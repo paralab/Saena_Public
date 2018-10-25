486eb9ac586ea666ded975708ddbf07d7519ee9e
majidrp <u1013493@utah.edu>
2018-10-24 23:35:03 -0600

    change and preallocate C_temp in fast_mm, change threshold for fast_mm

--- include/saena_object.h
+++ include/saena_object.h
@@ -39,6 +39,11 @@ public:
 //    bool shrink_cpu = true;
     bool dynamic_levels = true;
     bool adaptive_coarsening = true;
+
+    const index_t matmat_size_thre = 40000; // if(row * col) do the dense matmat default 40000
+//    const index_t min_size_threshold = 50; //default 50
+    const index_t matmat_nnz_thre = 100; //default 100
+
     bool doSparsify = false;
     std::string sparsifier = "majid"; // options: 1- TRSL, 2- drineas, majid
     double sparse_epsilon = 1;
@@ -79,13 +84,21 @@ public:
     int level_setup(Grid* grid);
     int coarsen(Grid *grid);
     int coarsen_old(Grid *grid);
+    int coarsen_update_Ac(Grid *grid, std::vector<cooEntry> &diff);
+//    int coarsen2(saena_matrix* A, prolong_matrix* P, restrict_matrix* R, saena_matrix* Ac);
+
     int fast_mm(cooEntry *A, cooEntry *B, std::vector<cooEntry> &C, nnz_t A_nnz, nnz_t B_nnz,
                 index_t A_row_size, index_t A_row_offset, index_t A_col_size, index_t A_col_offset,
                 index_t B_col_size, index_t B_col_offset,
                 index_t *nnzPerColScan_leftStart, index_t *nnzPerColScan_leftEnd,
-                index_t *nnzPerColScan_rightStart, index_t *nnzPerColScan_rightEnd, MPI_Comm comm);
-    int coarsen_update_Ac(Grid *grid, std::vector<cooEntry> &diff);
-//    int coarsen2(saena_matrix* A, prolong_matrix* P, restrict_matrix* R, saena_matrix* Ac);
+                index_t *nnzPerColScan_rightStart, index_t *nnzPerColScan_rightEnd,
+                value_t *mempool, MPI_Comm comm);
+    int fast_mm_row_col(cooEntry *A, cooEntry *B, std::vector<cooEntry> &C, nnz_t A_nnz, nnz_t B_nnz,
+                index_t A_row_size, index_t A_row_offset, index_t A_col_size, index_t A_col_offset,
+                index_t B_col_size, index_t B_col_offset,
+                index_t *nnzPerColScan_leftStart, index_t *nnzPerColScan_leftEnd,
+                index_t *nnzPerColScan_rightStart, index_t *nnzPerColScan_rightEnd,
+                value_t *mempool, MPI_Comm comm);
 
     int find_aggregation(saena_matrix* A, std::vector<unsigned long>& aggregate, std::vector<index_t>& splitNew);
     int create_strength_matrix(saena_matrix* A, strength_matrix* S);
