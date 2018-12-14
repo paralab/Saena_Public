cd967ce102241150a6b86fe7e6bf985511fc78f1
majidrp <u1013493@utah.edu>
2018-12-14 05:45:06 -0700

    add install script for nektar

---
+++ install.sh
@@ -0,0 +1,20 @@
+mkdir build && cd build
+make config prefix=`pwd` -C ../external/parmetis-4.0.3/metis
+cd build_metis
+make install
+cd ..
+make config prefix=`pwd` -C ../external/parmetis-4.0.3
+cd build_parmetis
+make install
+cd ..
+mkdir build_zfp && cd build_zfp
+cmake ../../external/zfp-0.5.3
+make -j28
+cd ..
+mkdir build_superlu && cd build_superlu
+cmake ../../external/SuperLU_DIST_5.4.0 \
+-DXSDK_INDEX_SIZE=64 \
+-Denable_blaslib=OFF \
+-DCMAKE_INSTALL_PREFIX=. ;\
+make -j28 install
+cd ..
